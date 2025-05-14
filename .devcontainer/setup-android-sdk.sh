45846ae53434cdde04b8e762a7a800357798d858
BaptisteHrmn <bh@fbbc.fr>
2025-05-14 11:13:26 +0000

    Ajout de la configuration Dev Container Android

--- .devcontainer/setup-android-sdk.sh
+++ .devcontainer/setup-android-sdk.sh
@@ -4,6 +4,7 @@ set -e
 ANDROID_SDK_ROOT=/usr/local/android-sdk
 TOOLS_VERSION=9477386
 
+<<<<<<< HEAD
 # Installer l’Android SDK command-line tools
 mkdir -p ${ANDROID_SDK_ROOT}/cmdline-tools
 curl -o sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-${TOOLS_VERSION}_latest.zip
@@ -18,3 +19,16 @@ yes | sdkmanager --sdk_root=${ANDROID_SDK_ROOT} --licenses
 
 # Installer la plateforme et build-tools
 sdkmanager --sdk_root=${ANDROID_SDK_ROOT} "platform-tools" "platforms;android-31" "build-tools;31.0.0"
+=======
+mkdir -p \${ANDROID_SDK_ROOT}/cmdline-tools
+curl -o sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-\${TOOLS_VERSION}_latest.zip
+unzip sdk.zip -d \${ANDROID_SDK_ROOT}/cmdline-tools
+rm sdk.zip
+mv \${ANDROID_SDK_ROOT}/cmdline-tools/cmdline-tools \${ANDROID_SDK_ROOT}/cmdline-tools/latest
+
+export ANDROID_SDK_ROOT=\${ANDROID_SDK_ROOT}
+export PATH=\$PATH:\${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin:\${ANDROID_SDK_ROOT}/platform-tools
+yes | sdkmanager --sdk_root=\${ANDROID_SDK_ROOT} --licenses
+
+sdkmanager --sdk_root=\${ANDROID_SDK_ROOT} "platform-tools" "platforms;android-31" "build-tools;31.0.0"
+>>>>>>> 4e30801 (Ajout de la configuration Dev Container Android)
